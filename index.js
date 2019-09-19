80566ced2460f48d424ac77652c3bb59b41f0a6d
adonis750 <adonis750@naver.com>
2019-09-19 17:06:34 +0900

    another changes

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log("test");
+console.log("edit");
+//debugger;
+
+(() => {
+    const arr = [1, 2];
+    for (let ele of arr) {
+        console.log(ele);
+    }
+})();
+
+console.log("d");
